c365b9399a00a6a5964efc2a5cff287ca8c68d38
Cris Kiri <cris.kiri@gmail.com>
2023-08-25 10:12:34 +1200

    feat: endpoints and proxy setup

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,10 +1,16 @@
 import request from 'superagent'
-import { Welcome } from '../models/welcome.ts'
+// import { Welcome } from '../models/welcome.ts'
 
 const serverURL = '/api/v1'
 
-// *** EXAMPLE ***
-export function getWelcome(): Promise<Welcome> {
-  return request.get(`${serverURL}/welcome`).then((response) => response.body)
+// // *** EXAMPLE ***
+// export function getWelcome(): Promise<Welcome> {
+//   return request.get(`${serverURL}/welcome`).then((response) => response.body)
+// }
+// // ***   ***   ***
+
+export async function getRandomSuperhero() {
+  const response = await request.get('/api/v1/superhero')
+
+  return response.body
 }
-// ***   ***   ***
